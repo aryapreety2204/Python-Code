c212730a778e4ac21a4c4c5bf9174a4c804d540d
Preety Arya <aryapreety48@gmail.com>
2024-06-04 23:20:30 +0530

    and one more code i prime number

--- basic programs/primeNumber.py
+++ basic programs/primeNumber.py
@@ -26,4 +26,41 @@ if(num>1):
 else:
   print(num,"is not prime number")
 
+# 3 Program to check if a number is prime or not
 
+num = int (input("enter a number:..."))
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
+
+# 4 
+num = int(input("enter a number:...."))
+flag = 0
+for i in range(2,num):
+  if num%i==0:
+    flag = 1
+    break
+if flag == 1:
+  print('Not Prime')
+else:
+  print("Prime")
